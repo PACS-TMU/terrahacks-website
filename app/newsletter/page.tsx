c724438d639ff0ef39ec941f9ced6b72db1cb806
grcng <diep.nguyen@torontomu.ca>
2025-05-18 19:32:37 -0400

    removed template files, fixed icons, created global.css and env.
    Co-authored-by: Isabel Kwon <IsabelKwon@users.noreply.github.com>
    Co-authored-by: Mo Nakouzi <mo.nakouzi03@gmail.com>

--- app/newsletter/page.tsx
+++ app/newsletter/page.tsx
@@ -1,8 +1,8 @@
 import { subscribeToNewsletterAction } from "@/app/actions";
 import { FormMessage, Message } from "@/components/form-message";
 import { SubmitButton } from "@/components/submit-button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { Input } from "@/components/form/input";
+import { Label } from "@/components/form/label";
 
 export default async function NewsletterPage({ 
   searchParams 
