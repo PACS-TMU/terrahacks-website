65fe17acb7f79ebd8c50bf95ff3741e2670a7752
grcng <diep.nguyen@torontomu.ca>
2025-05-18 19:30:08 -0400

    removed template files, fixed icons, created global.css and env.
    Co-authored-by: Isabel Kwon IsabelKwon@users.noreply.github.com
    Co-authored-by: Mo Nakouzi mo.nakouzi03@gmail.com

--- components/submit-button.tsx
+++ components/submit-button.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/form/button";
 import { type ComponentProps } from "react";
 import { useFormStatus } from "react-dom";
 
